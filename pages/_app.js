386652ef86e6a15bed5538153d047a611053f9f7
winedrop <lumiielumiie@gmail.com>
2023-02-25 21:17:30 -0800

    add and import styles.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
+import '@/styles/styles.css'
 import { SessionProvider } from 'next-auth/react';
+
+
 
 export default function App({ Component, pageProps }) {
   // pass higher order component by wrapping the rest of the app, meaning we can persist the login state while navigating app
