e3eec758ccd94c0b7bc1ee2b4bb724fa2fecade7
winedrop <lumiielumiie@gmail.com>
2023-03-04 21:00:01 -0800

    adds content to the homepage

--- pages/newhomepage.js
+++ pages/newhomepage.js
@@ -12,7 +12,8 @@ const navigation = [
     { name: 'about us', href: '#' },
     { name: 'users', href: '/users' },
     { name: 'new home page', href: '/newhomepage'},
-    { name: 'dashboard', href: '/dashboard'}
+    { name: 'dashboard', href: '/dashboard'},
+    { name: 'profile', href: '/users/1234/profile'}
   ]
 
 function homePage({ providers }) {
@@ -135,8 +136,59 @@ function homePage({ providers }) {
       {/* body */}
       <div className="h-max dark:bg-[#000000] pb-5">
         <div className="flex mx-auto flex-col w-8/12 align-middle gap-3">
+        <div className="relative px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            
+            <div className="text-center">
+              <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                pied piper 2.0
+              </h1>
+              <p className="mt-6 text-lg leading-8 text-cyan-400">
+                A social networking platform based on your music taste.
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <a
+                  href="#"
+                  className="rounded-md bg-sky-500 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Get started
+                </a>
+                <a href="#" className="text-base font-semibold leading-7 text-cyan-600">
+                  Learn more <span aria-hidden="true">→</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+            <svg
+              className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+              viewBox="0 0 1155 678"
+            >
+              <path
+                fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+                fillOpacity=".3"
+                d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+              />
+              <defs>
+                <linearGradient
+                  id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+                  x1="1155.49"
+                  x2="-78.208"
+                  y1=".177"
+                  y2="474.645"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#9089FC" />
+                  <stop offset={1} stopColor="#FF80B5" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>
+        </div>
           
-          
+        </div>
+        <div className="flex h-auto">
+
         </div>
       </div>
     </div>
